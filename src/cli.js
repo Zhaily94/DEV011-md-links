34bd5498c2cfc540d66aca5092f369a31f6c684f
zhaira <zhailysweetly@gmail.com>
2023-12-04 00:22:42 -0600

    estructura de cli, validaciones ruta, conversion

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,42 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
+import process from 'process';
+import fs from 'fs';
+import { mdLinks } from "./index.js";
 
-const yargs = require('yargs');
 
-yargs
-  .command({
-    command: 'saludar',
-    describe: 'Saluda al usuario',
-    handler: () => {
-      console.log('¡Hola! Bienvenido a mi CLI.');
-    }
-  })
-  .help()
-  .argv;
+// guarda la ruta escrita en consola
+const path = process.argv[2];
+//Comprueba que la ruta existe en el computador
+const validateLinksUser = fs.existsSync(path);
+const markdownExtensions = /\.(md|mkd|mdwn|mdown|mdtxt|mdtext|markdown|text)$/i;
+const validateTypeMD = markdownExtensions.test(path);
+
+if (validateLinksUser && validateTypeMD) {
+    //
+    mdLinks(path)
+    .then(result => console.log("Esta es la ruta", result))
+    .catch(err => console.error("Este es el error", err))
+    
+} else {
+    //
+    console.log('no existe la ruta')
+}
+
+
+// const route = 'C:/Users/ZhailyAlfa/Desktop/laboratoria-dev/DEV011-md-links/README.md'; 
+
+
+// const yargs = require('yargs');
+
+// yargs
+//   .command({
+//     command: 'saludar',
+//     describe: 'Saluda al usuario',
+//     handler: () => {
+//       console.log('¡Hola! Bienvenido a mi CLI.');
+//     }
+//   })
+//   .help()
+//   .argv;
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import { convertPath, mdLinks, readMarkdownRender } from './functions.js';
+import { convertPath } from './functions.js';
 
-const route = './readme.md'; 
-const pathAbs = convertPath(route); //
+// readMarkdownRender(pathAbs)
+//     .then((html) => {
+//         mdLinks(html)
+//             .then(result => console.log(result))
+//             .catch(err => console.error(err));
 
-readMarkdownRender(pathAbs)
-    .then((html) => {
-        mdLinks(html)
-            .then(result => console.log(result))
-            .catch(err => console.error(err));
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//     });
 
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+export function mdLinks(path) {
+  return new Promise((resolve, reject) => {
+    const rutaConvertida = convertPath(path);
 
+    resolve(rutaConvertida);
 
+    // const pathAbs = convertPath(route);
+  });
+}
