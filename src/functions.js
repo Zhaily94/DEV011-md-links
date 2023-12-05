0ac948a7441953bda38a720d57d18ac2babe43f0
Zhaira Lysset Santos Alfaro <52017903+Zhaily94@users.noreply.github.com>
2023-12-05 01:10:58 -0600

    was function findLinks added

--- src/functions.js
+++ src/functions.js
@@ -33,3 +33,44 @@ export function readMarkdownRender(path) {
     return html
   });
 }
+
+//mandarle el archivo md a la funcion
+// poner una condicional
+// valiadr que no este vacio o que si exista
+// del archivo dado modificarlo para que lo separe por lineas
+//guardarlo en una variable 
+// crear un patron para los href
+// recorrer el docuemnto 
+// comparar cada linea con el patron
+// si coincide guardarlo en un array
+
+export function findLinks(markdownHtml, path) {
+  
+  const links = [];
+  const hrefRegex = /https?:\/\/[^\s]+/g; // extrae links del md
+  const deleteHtmlRegex = /<[^>]+>/g; // patron para extraer etiquetas html
+ 
+  if(!markdownHtml){ //validacion de md vacio
+    return links;
+  }
+
+  const lines = markdownHtml.split('\n');  // Dividir el texto en líneas
+  lines.forEach((line) => { // recorre cada linea
+    const href = line.match(hrefRegex); // hace match de la linea con la expresion de extraer links y los guarda en href
+    const title = line.replace(deleteHtmlRegex, ''); // Elimina las etiquetas HTML solo nos trae el texto de cada enlace
+    if (href && title) { // comprueba que existan href y titulo
+      links.push( // guarda los elementos traidos en un array links
+        {
+          href: href[0].replace(/".*>/, ''),
+          title: title,
+          file: path
+        }
+      );
+    }
+  });
+  return links;
+}
+
+
+
+
