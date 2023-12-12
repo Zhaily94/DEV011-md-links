8958bcab9221d4cf84caa5c039cb36781de7c677
zhaira <zhailysweetly@gmail.com>
2023-12-12 00:32:10 -0600

    was functions validateLinks added and library axios

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 
 // version commonJs
-const { convertPath, readMarkdownRender, findLinks } = require('./functions.js');
+const { convertPath, readMarkdownRender, findLinks, validateLinks } = require('./functions.js');
+// const { convertPath, readMarkdownRender, findLinks, validateLinks } = require('./functions.js');
 
-function mdLinks(path) {
-  return new Promise((resolve, reject) => {
-    const rutaConvertida = convertPath(path);
+function mdLinks(path, validate) {
+
+  const rutaConvertida = convertPath(path);
+  const promiseArrayLinks = new Promise((resolve, reject) => {
     readMarkdownRender(rutaConvertida)
       .then((html) => {
         const linksExtraidos = findLinks(html, rutaConvertida);
         resolve(linksExtraidos);
       })
       .catch((error) => {
-        // Manejar el error
         console.error('Error al leer el markdown:', error);
         reject(error);
       });
   });
+
+    if (validate === 'true') {
+    return promiseArrayLinks.then((array) => {
+      const arrayValidate = validateLinks(array);
+      return arrayValidate;
+    }).catch((error) => {
+      console.error('Error al validar los links:', error);
+      throw error;
+    });
+  } else if (validate === 'false' || validate === ''){
+    return promiseArrayLinks.catch((error) => {
+      console.error('Error al leer el markdown:', error);
+      throw error;
+    });
+  } else {
+    reject(new Error('Error'));
+  }
+
+  // if (validate === 'true') {
+  //   return promiseArrayLinks.then((array) => {
+  //     const arrayValidate = validateLinks(array);
+  //     return arrayValidate;
+  //   }).catch((error) => {
+  //     console.error('Error al validar los links:', error);
+  //     throw error;
+  //   });
+  // } else {
+  //   return promiseArrayLinks.catch((error) => {
+  //     console.error('Error al leer el markdown:', error);
+  //     throw error;
+  //   });
+  // }
 }
 
 module.exports = { mdLinks };
